Fixed resource leakage with multiprocessing.shared_memory

stream_processing/sharedmem_stream_processing_app.py
@@ -21,10 +21,13 @@ class SharedmemStreamProcessingApp(WebcamApp):
         self.logger.info("Initializing SharedmemStreamProcessingApp")
         self.transform = transform
 
+        self.resolution_set_event = None
+        self.frame_buffer_shm = None
+        self.frame_shape_shm = None
+
         self.webcam_to_processing_queue = None
         self.processing_to_webcam_queue = None
         self.exit_flag = None
-        self.shm_event = None
         self.shared_mem_lock = None
 
     def webcam_reader_loop(self):
@@ -39,24 +42,25 @@ class SharedmemStreamProcessingApp(WebcamApp):
         # https://stackoverflow.com/questions/66353952/how-to-pass-video-stream-from-one-python-script-to-another/66522825#66522825
         ##
 
-        frame_buffer_shm = SharedMemory(name='cur_frame', create=True, size=width * height * 3)
-        frame = np.ndarray((height, width, 3), buffer=frame_buffer_shm.buf, dtype='u1')
-        frame_shape_shm = SharedMemory(name="frame_shape", create=True, size=3*4) #4 bytes per dim as long as int32 is big enough
-        self.shm_event.set()
-        frame_shape = np.ndarray(3, buffer=frame_shape_shm.buf, dtype='i4')  #4 bytes per dim as long as int32 is big enough
-        frame_shape[:] = np.array([height, width, 3])
+        frame_shape_shm = SharedMemory(name="frame_shape")
+        frame_shape = np.ndarray(2, buffer=frame_shape_shm.buf, dtype='i4')
+        frame_shape[:] = np.array([height, width])
+        self.resolution_set_event.set()
+        self.frame_buffer_init_event.wait()
+        frame_shm = SharedMemory(name='cur_frame')
+        frame = np.ndarray((*frame_shape, 3), buffer=frame_shm.buf, dtype='u1')
 
         while True:
             if self.exit_flag.value:
                 break
 
-            ret, a = cap.read(frame)
+            ret, _ = cap.read(frame)
             frame[:] = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cap.release()
         self.webcam_to_processing_queue.cancel_join_thread()
         self.processing_to_webcam_queue.cancel_join_thread()
         frame_shape_shm.close()
-        frame_buffer_shm.close()
+        frame_shm.close()
     def opencv_window_loop(self):
         self.logger.info("Starting opencv window loop")
         prev_time = time.time()
@@ -83,11 +87,11 @@ class SharedmemStreamProcessingApp(WebcamApp):
 
     def image_process_loop(self):
         self.logger.info("Starting image process loop")
-        self.shm_event.wait()
+        self.frame_buffer_init_event.wait()
         frame_shape_shm = SharedMemory(name="frame_shape")
-        frame_shape = np.ndarray(3, buffer=frame_shape_shm.buf, dtype='i4')
+        frame_shape = np.ndarray(2, buffer=frame_shape_shm.buf, dtype='i4')
         frame_shm = SharedMemory(name='cur_frame')
-        frame = np.ndarray(frame_shape, buffer=frame_shm.buf, dtype='u1')
+        frame = np.ndarray((*frame_shape, 3), buffer=frame_shm.buf, dtype='u1')
         while True:
             if self.exit_flag.value:
                 break
@@ -103,8 +107,15 @@ class SharedmemStreamProcessingApp(WebcamApp):
         self.webcam_to_processing_queue = multiprocessing.Queue()
         self.processing_to_webcam_queue = multiprocessing.Queue()
         self.exit_flag = multiprocessing.Value(ctypes.c_bool, False)
-        self.shm_event = multiprocessing.Event()
         self.shared_mem_lock = multiprocessing.Lock()
+
+        self.resolution_set_event = multiprocessing.Event()
+        self.frame_buffer_init_event = multiprocessing.Event()
+
+
+        frame_shape_shm = SharedMemory(name="frame_shape", create=True, size=3*4) #4 bytes per dim as long as int32 is big enough
+        frame_shape = np.ndarray(2, buffer=frame_shape_shm.buf, dtype='i4')  #4 bytes per dim as long as int32 is big enough
+
 
         transformation_process = multiprocessing.Process(
             target=self.image_process_loop,
@@ -116,7 +127,17 @@ class SharedmemStreamProcessingApp(WebcamApp):
         )
 
         webcam_reader_process.start()
+        self.resolution_set_event.wait()
+
+        frame_buffer_shm = SharedMemory(name='cur_frame', create=True, size=frame_shape[0] * frame_shape[1] * 3)
+        self.frame_buffer_init_event.set()
+
         transformation_process.start()
         self.opencv_window_loop()
         transformation_process.join()
         webcam_reader_process.join()
+
+        frame_buffer_shm.close()
+        frame_shape_shm.close()
+        frame_buffer_shm.unlink()
+        frame_shape_shm.unlink()