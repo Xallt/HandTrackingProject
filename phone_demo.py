b1f8f53bff9da65f553b54410067cd358e45a668
Dmitry Shabat <mitya.shabat@gmail.com>
2023-07-26 12:06:25 +0400

    Option to use any of the impemented apps

--- phone_demo.py
+++ phone_demo.py
@@ -1,6 +1,5 @@
 import argparse
 from transformations.mp_hand_detection import ImageTransform, MediaPipeHandDetectionTransform
-from stream_processing.stream_processing_app import StreamProcessingApp
 
 
 if __name__ == '__main__':
@@ -8,13 +7,39 @@ if __name__ == '__main__':
 
     parser.add_argument('url', type=str, help='URL of video stream')
     parser.add_argument('--debug', action='store_true', help='Run in debug mode')
+    parser.add_argument(
+        '--app_type',
+        choices=[
+            'sync',                   # Synchronous mode
+            'async_queue',            # Asynchronous mode
+            'async_sharedmem',        # Asynchronous mode with shared memory
+        ],
+        default='sync',
+        help='Type of app to run'
+    )
 
     args = parser.parse_args()
 
-    app = StreamProcessingApp(
-        args.url,
-        MediaPipeHandDetectionTransform(),
-        debug=args.debug
-    )
+    if args.app_type == 'sync':
+        from stream_processing.sync_stream_processing_app import SynchronousStreamProcessingApp
+        app = SynchronousStreamProcessingApp(
+            args.url,
+            MediaPipeHandDetectionTransform(),
+            debug=args.debug
+        )
+    elif args.app_type == 'async_queue':
+        from stream_processing.stream_processing_app import StreamProcessingApp
+        app = StreamProcessingApp(
+            args.url,
+            MediaPipeHandDetectionTransform(),
+            debug=args.debug
+        )
+    elif args.app_type == 'async_sharedmem':
+        from stream_processing.sharedmem_stream_processing_app import SharedmemStreamProcessingApp
+        app = SharedmemStreamProcessingApp(
+            args.url,
+            MediaPipeHandDetectionTransform(),
+            debug=args.debug
+        )
     app.run()
 
