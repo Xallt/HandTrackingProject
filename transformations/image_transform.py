d5f2d035e74579ffc08e6faf4bcef551f18fdf8a
Dmitry Shabat <mitya.shabat@gmail.com>
2023-08-11 22:32:27 +0400

    Implement a defaullt async invoking of ImageTransform

--- transformations/image_transform.py
+++ transformations/image_transform.py
@@ -6,3 +6,11 @@ class ImageTransform:
     """
     def transform(self, image: np.ndarray) -> np.ndarray:
         raise NotImplementedError("transform() not implemented")
+    def set_callback(self, callback):
+        self.callback = callback
+    def transform_async(self, image: np.ndarray, timestamp: float):
+        """
+        Asynchronous transform of image
+        Default implementation just calls the transform() method
+        """
+        self.callback(self.transform(image))
