62f64b5e41c3c5baad7fb16e0e9092fb709bf7e9
Dmitry <mitya.shabat@gmail.com>
2023-10-04 19:48:09 +0400

    Basic example with opencv import

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
+    cout << "I want you to give me an image path and I'll read the image for you" << endl;
     string s;
     cin >> s;
-    cout << "Hello, " << s << "!\n";
+    cv::Mat img = cv::imread(s);
+    if (img.empty()) {
+        std::cerr << "Error: Could not read the image" << std::endl;
+        return 1;
+    }
+    // Image read successfully. Do nothing.
+    return 0;
 }
