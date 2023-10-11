480a7eb9bb3d8edd49cede78368c40acfff7baaa
Dmitry <mitya.shabat@gmail.com>
2023-10-11 20:30:46 +0400

    Use the mp_hands.Hands solution class

--- python_app/transformations/mp_hand_detection.py
+++ python_app/transformations/mp_hand_detection.py
@@ -5,89 +5,43 @@ from transformations.image_transform import ImageTransform
 import pyautogui
 import numpy as np
 import cv2
+from PIL import Image
 
-SCREEN_SIZE = pyautogui.size()
-HAND_LANDMARKER_PATH = 'assets/mediapipe/hand_landmarker.task'
-
-BaseOptions = mp.tasks.BaseOptions
-HandLandmarker = mp.tasks.vision.HandLandmarker
-HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
-HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
-
-# From https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/hand_landmarker/python/hand_landmarker.ipynb#scrollTo=s3E6NFV-00Qt&uniqifier=1
-def draw_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-
-    height, width, _ = rgb_image.shape
-    # Loop through the detected hands to visualize.
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-        ])
-        solutions.drawing_utils.draw_landmarks(
-            rgb_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style())
-
-        # Get the top left corner of the detected hand's bounding box.
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-
-        # Draw handedness (left or right hand) on the image.
-        cv2.putText(rgb_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 class MediaPipeHandDetectionTransform(ImageTransform):
     def __init__(self):
-        self.sync_options = HandLandmarkerOptions(
-            base_options=BaseOptions(model_asset_path=HAND_LANDMARKER_PATH),
-            running_mode=VisionRunningMode.IMAGE,
-            num_hands=2
-        )
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_hands = mp.solutions.hands
-
-    def _draw_landmark_callback(self, result, image, timestamp):
-        image = image.numpy_view()
-        draw_landmarks_on_image(image, result)
-        self.callback(image)
+        self.hands = None
 
     def set_callback(self, callback):
         self.callback = callback
-        self.options_async = HandLandmarkerOptions(
-            base_options=BaseOptions(model_asset_path=HAND_LANDMARKER_PATH),
-            running_mode=VisionRunningMode.LIVE_STREAM,
-            result_callback=self._draw_landmark_callback,
-            num_hands=2
-        )
 
     def transform_async(self, image, timestamp):
-        image = image.copy()
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-        with HandLandmarker.create_from_options(self.options_async) as landmarker:
-            landmarker.detect_async(mp_image, int(timestamp))
+        image = self.transform(image)
+        self.callback(image)
     def transform(self, image):
+        # Initialize the solution class in the first call to transform
+        # Otherwise it will be copied across processes and that breaks the solution
+        if self.hands is None:
+            self.hands = mp_hands.Hands(
+                static_image_mode=False,
+                max_num_hands = 2
+            )
+
+
         image = image.copy()
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-        with HandLandmarker.create_from_options(self.sync_options) as landmarker:
-            results = landmarker.detect(mp_image)
-        draw_landmarks_on_image(image, results)
+        hands_result = self.hands.process(image)
+        if hands_result.multi_hand_landmarks is None:
+            return image
+        for hand_landmarks in hands_result.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style()
+            )
         return image
+
